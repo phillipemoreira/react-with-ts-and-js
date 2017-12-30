51941b40988951b4f546d21c511bd79f125612c4
phillipemoreira <phillipe.moreira@quintoandar.com.br>
2017-12-30 17:34:30 -0200

    transforming app into js and importing both js ans tsx files

--- app/app.js
+++ app/app.js
@@ -1,18 +1,21 @@
-import * as React from "react"
+import React from "react"
 import { render } from "react-dom";
 import darkBaseTheme from 'material-ui/styles/baseThemes/lightBaseTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
-import DatePicker from "./components/DatePicker/index";
 
-class App extends React.Component<{}, {}> {
+import DatePicker from "./components/DatePicker/index";
+import DatePickerJS from "./components/DatePickerJS/index.js";
+
+class App extends React.Component {
   render() {
     return (
       <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
         <DatePicker />
+        <DatePickerJS />
       </MuiThemeProvider>
     );
   }
 }
 
-render(<App />, document.getElementById("root") as HTMLElement);
+render(<App />, document.getElementById("root"));
---
+++ app/components/DatePickerJS/index.js
@@ -0,0 +1,10 @@
+import React from "react"
+import DatePicker from 'material-ui/DatePicker';
+
+const DatePickerExampleSimple = () => (
+  <div>
+    <DatePicker hintText="Portrait Dialog" />
+  </div>
+);
+
+export default DatePickerExampleSimple;

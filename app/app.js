a955a7dedc269bdd385f28a412ffa55c71b50c72
phillipemoreira <phillipe.moreira@quintoandar.com.br>
2017-12-31 13:29:54 -0200

    initial eslint packages and rules

--- app/app.js
+++ app/app.js
@@ -1,11 +1,11 @@
-import React from "react"
-import { render } from "react-dom";
+import React from 'react';
+import { render } from 'react-dom';
 import darkBaseTheme from 'material-ui/styles/baseThemes/lightBaseTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 
-import DatePicker from "./components/DatePicker/index";
-import DatePickerJS from "./components/DatePickerJS/index.js";
+import DatePicker from './components/DatePicker/index';
+import DatePickerJS from './components/DatePickerJS/index.js';
 
 class App extends React.Component {
   render() {
@@ -18,4 +18,4 @@ class App extends React.Component {
   }
 }
 
-render(<App />, document.getElementById("root"));
+render(<App />, document.getElementById('root'));

39d91373a27abbff7a4d821101df70483e35dac7
phillipemoreira <phillipe.moreira@quintoandar.com.br>
2017-12-31 17:49:53 -0200

    using logger and accepting custom port

--- server/index.js
+++ server/index.js
@@ -1,10 +1,23 @@
+/* eslint consistent-return:0 */
+
 const express = require('express');
+const logger = require('./logger');
 const { resolve } = require('path');
+const argv = require('minimist')(process.argv.slice(2));
 
 const app = express();
 
+// serving build folder
 app.use('/', express.static(resolve(process.cwd(), 'build')));
 
-app.listen(7000, () => {
-  console.log('Example app listening on port 7000!');
+// get the intended port number
+const port = argv.port || process.env.PORT || 4000;
+
+// Start your app.
+app.listen(port, (err) => {
+  if (err) {
+    return logger.error(err.message);
+  }
+
+  logger.appStarted(port, 'localhost');
 });

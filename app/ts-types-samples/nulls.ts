c94fad5a20e1872418dc175bf21cd8fb4d024e1a
phillipemoreira <phillipe.moreira@quintoandar.com.br>
2018-01-02 17:38:33 -0200

    renaming folders

--- app/ts-types-samples/nulls.ts
+++ app/ts-types-samples/nulls.ts
@@ -1,5 +1,9 @@
 let cannotBeNull = 12;
 // cannotBeNull = null; TS2322: Type 'null' is not assignable to type 'number'.
 
+let canBeNull: number | null;
+canBeNull = 12;
+canBeNull = null;
+
 let canBeNullBecauseItIsUndef;
 canBeNullBecauseItIsUndef = null;
